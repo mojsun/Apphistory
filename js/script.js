cebcc85f0ea0713a90cc0c5e21600ae0f6b855c5
mojsun <m.khorashahi7@gmail.com>
2024-11-09 13:16:44 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,91 @@
+// Add custom JavaScript here
+function userScroll() {
+  const navbar = document.querySelector(".navbar");
+  const toTopBtn = document.querySelector("#to-top");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("navbar-sticky");
+      toTopBtn.classList.add("show");
+    } else {
+      navbar.classList.remove("navbar-sticky");
+      toTopBtn.classList.remove("show");
+    }
+  });
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function incrementStats() {
+  const counters = document.querySelectorAll(".counter");
+
+  counters.forEach((counter) => {
+    counter.innerText = 0;
+
+    const updateCounter = () => {
+      const target = +counter.getAttribute("data-target");
+      const c = +counter.innerText;
+
+      const increment = target / 200;
+
+      if (c < target) {
+        counter.innerText = Math.ceil(c + increment);
+        setTimeout(updateCounter, 1);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+// Event Listeners
+document.addEventListener("DOMContentLoaded", userScroll);
+document.addEventListener("DOMContentLoaded", incrementStats);
+document.querySelector("#to-top").addEventListener("click", scrollToTop);
+
+// Video Modal
+const videoBtn = document.querySelector(".video-btn");
+const videoModal = document.querySelector("#videoModal");
+const video = document.querySelector("#video");
+let videoSrc;
+
+if (videoBtn !== null) {
+  videoBtn.addEventListener("click", () => {
+    videoSrc = videoBtn.getAttribute("data-bs-src");
+  });
+}
+
+if (videoModal !== null) {
+  videoModal.addEventListener("shown.bs.modal", () => {
+    video.setAttribute(
+      "src",
+      videoSrc + "&autoplay=1&modestbranding=1&showinfo=0"
+    );
+  });
+
+  videoModal.addEventListener("hide.bs.modal", () => {
+    video.setAttribute("src", ""); // Clear the src to stop playback
+  });
+}
+
+// Replace Text In Header
+const checkReplace = document.querySelector(".replace-me");
+
+if (checkReplace !== null) {
+  const replace = new ReplaceMe(checkReplace, {
+    animation: "animated fadeIn",
+    speed: 2000,
+    separator: ",",
+    loopCount: "infinite",
+    autoRun: true,
+  });
+}
+///sign up login ///////////
+// document.querySelector(".img__btn").addEventListener("click", function () {
+//   document.querySelector(".cont").classList.toggle("s--signup");
+// });
